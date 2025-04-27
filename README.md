0209da5df549b3fa97510fe20e9af545bef611eb
DebugJedi <priyankrao2512@gmail.com>
2025-04-27 17:45:00 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## A Refresher of C (For my use....')
+Hi Thank you for stubbling on my profile. 
+This would be a series on refresher on the C as the Tile already suggested. This would be my first time testing something like Posting progress sort of on my task. Let's go:
+
+1. [Basic Structure] (basicstructure.c)
+2. 
---
+++ basicstructure.c
@@ -0,0 +1,31 @@
+#include <stdio.h> //Preprocessot directive
+
+int main() { //Main function
+    printf("Hello, World!\n"); //Printsto the console
+    return 0; //Exit code
+
+}
+
+
+// First line
+/*
+    #include <stdio.h> //Preprocessor directive
+    Includes the Standard Input Output functions library, which is necessary for using printf and other I/O functions.
+*/  
+
+// Second line
+/*
+    int main() { //Main function
+    Every C program status from main().
+*/
+// Third line
+/*
+    printf -> Prints text.
+*/
+
+// Fourth line
+/*
+    return 0; //Exit code
+    Program ends properly.
+*/
+
